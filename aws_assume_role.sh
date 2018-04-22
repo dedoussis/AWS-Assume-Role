dc75cb93cf5dc7c6c476e734a14e7bd94f7b8841
Dimitrios Dedoussis <ddedoussis@gmail.com>
2018-04-22 20:21:54 +0100

    Associative array and further clean up

--- aws_assume_role.sh
+++ aws_assume_role.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 if [ -z $role_arn ]
   then
@@ -15,28 +15,32 @@ echo "Please enter your MFA token code:"
 read token_code
 
 caller_id=(`aws sts get-caller-identity --output text`)
+account_id=$caller_id[1]
 user=(`echo $caller_id | cut -d '/' -f 2`)
 username=$user[1]
-account_id=$caller_id[1]
 
-aws_credentials=(`aws sts assume-role --role-arn $role_arn --role-session-name test --serial-number arn:aws:iam::$account_id:mfa/$username --token-code $token_code --output text`)
-access_key_id_index=5
-secret_access_key_index=7
-session_token_index=8
+aws_credentials=(`aws sts assume-role \
+                 --role-arn $role_arn \
+                 --role-session-name $username-session \
+                 --serial-number arn:aws:iam::$account_id:mfa/$username \
+                 --token-code $token_code \
+                 --output text`)
+
+declare -A indexes && indexes=( access_key_id 5 secret_access_key 7 session_token 8 )
 
 ITER=1
 for word in $aws_credentials 
 do
     case $ITER in
-    $access_key_id_index)
+    ${indexes[access_key_id]})
         export AWS_ACCESS_KEY_ID=$word
         echo "Exported AWS_ACCESS_KEY_ID"
         ;;
-    $secret_access_key_index)
+    ${indexes[secret_access_key]})
         export AWS_SECRET_ACCESS_KEY=$word
         echo "Exported AWS_SECRET_ACCESS_KEY"
         ;;
-    $session_token_index)
+    ${indexes[session_token]})
         export AWS_SESSION_TOKEN=$word
         echo "Exported AWS_SESSION_TOKEN"
         ;;
